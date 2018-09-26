a1a5b910bcff3522434ae6d4671b205d54eb0e6b
Anton Koshkin <akoshkin@devexperts.com>
2018-09-26 22:16:49 +0300

    karabiner changes

--- apps/karabiner/map.sh
+++ apps/karabiner/map.sh
@@ -1,4 +1,4 @@
 # makes senlink to my settings file
-ln -s ~/projects/dotfiles/app/karabiner ~/.config
+cp karabiner.json ~/.config/karabiner/karabiner.json
 # restarts karabiner_console_user_server
 launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
